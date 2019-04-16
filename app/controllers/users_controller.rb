8b88e99e5a24b0303de21344989c3eac43c55313
LanMaiThiNguyen14 <nguyenlan.orchids@gmail.com>
2019-04-16 10:23:46 +0700

    chapter8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,18 +3,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def show
-    @user = User.find_by id: params[:id]
-  end
-
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      log_in @user
+      flash[:success] = t(".new.success_ac")
       redirect_to @user
     else
-      render "new"
+      render :new
     end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+    redirect_to root_path
+    flash[:danger] = t(".error")
   end
 
   private
